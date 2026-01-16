65cd96156e43fd461c9f90832bc6db6099b84738
ganesha2208 <ganeshadream11@gmail.com>
2026-01-16 17:13:29 +0530

    app.js updated

--- hello-ganesha/src/App.js
+++ hello-ganesha/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Hello Ganesha.... BABY How are YOU!!!
-      </h1>
+    <div className="app-container">
+      <div className="card">
+        <h1 className="title">
+          Hello <span>Ganesha</span> 👋
+        </h1>
+
+        <p className="subtitle">
+          BABY, How are YOU!!! 😄
+        </p>
+
+        <button className="btn">
+          Get Started 🚀
+        </button>
+      </div>
     </div>
   );
 }
