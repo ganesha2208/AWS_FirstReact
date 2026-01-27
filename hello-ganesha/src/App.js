565cf2916b6562a672b54145a1ae8b6eeb21a80c
Ubuntu <ubuntu@ip-172-31-45-69.eu-north-1.compute.internal>
2026-01-27 09:31:35 +0000

    App.js updated

--- hello-ganesha/src/App.js
+++ hello-ganesha/src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 React CI/CD Pipeline</h1>
       <h2>Deployed using Jenkins & Docker</h2>
+	  <h1>Hello Ganesha baby...</h1>
       <p>Build Time: {new Date().toString()}</p>
     </div>
   );
