c5eec2a634a61bc7f3f9d938887f6f31d38e6eed
ganesha2208 <114673520+ganesha2208@users.noreply.github.com>
2026-05-13 18:51:38 +0530

    Update App.js

--- hello-ganesha/src/App.js
+++ hello-ganesha/src/App.js
@@ -3,7 +3,7 @@ function App() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>🚀 React CI/CD Pipeline</h1>
       <h2>Deployed using Jenkins & Docker</h2>
-	  <h1>Hello Ganesha baby...</h1>
+	  <h1>Hello Ganesha baby how are you...</h1>
       <p>Build Time: {new Date().toString()}</p>
     </div>
   );
